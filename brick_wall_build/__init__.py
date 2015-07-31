c24125ddff80015599048691fa8fe27680cb2474
Artem Onuchin <artem@indeed.com>
2015-07-31 09:25:27 +0900

    Bumped version

--- brick_wall_build/__init__.py
+++ brick_wall_build/__init__.py
@@ -2,7 +2,7 @@
 Lightweight Python Build Tool
 """
 
-__version__ = "0.0.1a.dev3"
+__version__ = "0.0.1a"
 __license__ = "MIT License"
 __contact__ = "https://github.com/IndeedTokyo-BrickWall/brick-wall-build"
 from ._brick_wall_build import task, main, set_artifact_path, set_tmp_path, run
